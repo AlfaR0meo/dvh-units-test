a20cca60ab4326be416a49cfecac18c23bfe2113
AlfaR0meo <80147573+AlfaR0meo@users.noreply.github.com>
2023-04-28 09:11:47 +0300

    better solution with dataset

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const sections = document.querySelectorAll('section');
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         sections.forEach(section => {
-            section.style.height = button.dataset.dvh + 'dvh';
+            section.style.height = button.dataset.vh;
         })
     });
 });
